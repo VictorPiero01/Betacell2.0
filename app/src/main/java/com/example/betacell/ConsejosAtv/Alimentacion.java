d409b5e95cdf2af869c152eebcff864bdf851715
Victor Piero Limache Victorio <71676154+VictorPiero01@users.noreply.github.com>
2020-12-17 17:52:25 -0500

    vplv

--- app/src/main/java/com/example/betacell/ConsejosAtv/Alimentacion.java
+++ app/src/main/java/com/example/betacell/ConsejosAtv/Alimentacion.java
@@ -11,7 +11,6 @@ public class Alimentacion extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_alimentacion);
-
+        
     }
 }
